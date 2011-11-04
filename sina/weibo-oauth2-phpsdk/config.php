30423f18d738b81c12475378cbf727ed119bed79
guo xiangke <xiangkeguo@gmail.com>
2011-11-04 22:58:55 +0800

    update1104-2256

---
+++ sina/weibo-oauth2-phpsdk/config.php
@@ -0,0 +1,5 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+define( "WB_AKEY" , 'xxxxxxxxx' );
+define( "WB_SKEY" , 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' );
+define( "CANVAS_PAGE" , "http://apps.weibo.com/xxxxxxxx" );
