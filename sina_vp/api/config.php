2edf4d4d7a8b8776fb9e209347f57a55b2837d6e
guo xiangke <xiangkeguo@gmail.com>
2011-11-07 20:44:30 +0800

    1107commit

--- sina_vp/api/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define( "WB_AKEY" , '221061042' );
-define( "WB_SKEY" , '11714bde45a7606c1b25c6c7ae72a638' );
-
-?>
--- sina_vp/weibo-oauth2-phpsdk/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Content-Type: text/html; charset=UTF-8');
-define( "WB_AKEY" , 'xxxxxxxxx' );
-define( "WB_SKEY" , 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' );
-define( "CANVAS_PAGE" , "http://apps.weibo.com/xxxxxxxx" );
