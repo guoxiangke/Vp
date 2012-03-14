230ba4cbc2afeeba4dc76b5362641f72b9d1790d
guo xiangke <xiangkeguo@gmail.com>
2012-03-14 08:35:22 +0800

    120313

--- sina_vp_imagetool/js/izhuanjiao.js
+++ sina_vp_imagetool/js/izhuanjiao.js
@@ -7,69 +7,6 @@
 		$(this).css("opacity","0.8");
 	}).mouseout(function(){
 		$(this).css("opacity","1");
-	}).mousemove(function(event){
-				if(flag==false){
-				}else if(flag=true){
-					//初始化数据
-					var e =  event || window.event ;
-					var obj=$(this);			
-					//获取图片的展示宽高，
-					var img_obj=new Image();
-					img_obj.src=obj.attr('src');
-					image_width=img_obj.width;
-					image_height=img_obj.height;
-
-					//获取鼠标坐标
-					mouse_x=e.offsetX || e.originalEvent.layerX;
-					mouse_y=e.offsetY || event.originalEvent.layerY;
-
-					
-					var get_mouse_x=obj.attr('mouse_x');
-					var get_mouse_y=obj.attr('mouse_y');
-					if(get_mouse_x==null){
-						obj.attr('mouse_x',mouse_x);
-						obj.attr('mouse_y',mouse_y);	
-					}else{
-						var mouse_move_x=mouse_x-get_mouse_x;
-						var mouse_move_y=mouse_y-get_mouse_y;
-						mouse_x=e.offsetX || e.originalEvent.layerX;
-						mouse_y=e.offsetY || event.originalEvent.layerY;
-						obj.attr('mouse_x',mouse_x);
-						obj.attr('mouse_y',mouse_y);
-
-						if(image_height/image_width>1){
-							var limit_y=obj.attr('height')-box_height;
-							var image_move_y=mouse_move_y;
-							//image_move_y
-							var image_margin_top=parseInt(obj.css('margin-top'))+image_move_y;
-							
-
-							
-							if(-(limit_y)<image_margin_top && image_margin_top<0){
-								
-								obj.css('margin-top',image_margin_top);
-								//alert(limit_y+" "+image_margin_top);
-							}
-						}else if(image_width/image_height>1){
-							var limit_x=obj.attr('width')-box_width;
-							var image_move_x=mouse_move_x;
-							//image_move_y
-							var image_margin_left=parseInt(obj.css('margin-left'))+image_move_x;
-							
-
-							
-							if(-(limit_x)<image_margin_left && image_margin_left<0){
-								
-								obj.css('margin-left',image_margin_left);
-								//alert(limit_y+" "+image_margin_top);
-							}
-						}
-					}
-
-														
-					
-									
-				}
 	}).click(function(e){
 				
 				var $obj=$(this);
@@ -90,17 +27,71 @@
 
 				if(image_orignal_width>box_width || image_orignal_height>box_height){
 					if(image_orignal_height/image_orignal_width>1){
-						var limit_size=image_orignal_width/box_width;
-						obj.attr('width',box_width);
-						obj.attr('height',image_orignal_height/limit_size);
-
+						obj.css('max-width',box_width);
 					}else if(image_orignal_height/image_orignal_width<=1){
-						var limit_size=image_orignal_height/box_height;
-						obj.attr('height',box_height);
-						obj.attr('width',image_orignal_width/limit_size);					
+						obj.css('max-height',box_height);
 					}
 				}
 
+			}).mousemove(function(event){
+				if(flag==false){
+				}else if(flag=true){
+					//初始化数据
+					var e =  event || window.event ;
+					var obj=$(this);			
+					//获取图片的展示宽高，
+					image_width=obj.width();
+					image_height=obj.height();
+					//获取图片真实的宽高
+					var img_obj=new Image();
+					img_obj.src=obj.attr('src');
+					image_real_width=img_obj.width;
+					image_real_height=img_obj.height;
+				}
+					//获取鼠标坐标
+					mouse_x=e.offsetX || e.originalEvent.layerX;
+					mouse_y=e.offsetY || event.originalEvent.layerY;
+					var get_mouse_x=obj.attr('mouse_x');
+					var get_mouse_y=obj.attr('mouse_y');
+					if(get_mouse_x==null){
+						obj.attr('mouse_x',mouse_x);
+						obj.attr('mouse_y',mouse_y);	
+					}else{
+						var mouse_move_x=-(mouse_x-get_mouse_x);
+						var mouse_move_y=-(mouse_y-get_mouse_y);
+						mouse_x=e.offsetX || e.originalEvent.layerX;
+						mouse_y=e.offsetY || event.originalEvent.layerY;
+						obj.attr('mouse_x',mouse_x);
+						obj.attr('mouse_y',mouse_y);
+					
+					if(image_height/image_width>1){
+						var limit_y=obj.height()-box_height;
+						var image_move_y=mouse_move_y*(image_real_height/box_height);
+						var image_margin_top=parseInt(obj.css('margin-top'))+image_move_y;
+							
+
+							
+						if(-(limit_y)<image_margin_top && image_margin_top<0){
+								
+							obj.css('margin-top',image_margin_top);
+							//alert(limit_y+" "+image_margin_top);
+						}
+					}else if(image_width/image_height>1){
+						var limit_x=obj.width()-box_width;
+						var image_move_x=mouse_move_x*(image_real_width/box_width);
+						//image_move_y
+						var image_margin_left=parseInt(obj.css('margin-left'))+image_move_x;
+							
+
+							
+						if(-(limit_x)<image_margin_left && image_margin_left<0){
+								
+							obj.css('margin-left',image_margin_left);
+							//alert(limit_y+" "+image_margin_top);
+						}
+					}
+
+					}
 			})
 			$('.normal-image-hidden').click(function(e){
 				var $obj=$(this);
--- sina_vp_imagetool/js/likeload.js
+++ sina_vp_imagetool/js/likeload.js
@@ -8,7 +8,7 @@
 		var userPicsrc=$('.user-img img').attr('src');
 		var userName=$('.user-name p').text();
 		//alert(userPicsrc);
-		words="<div class='logi_box'><a id='comment-272'></a><div id='272' class='clear' rel='272'><div class='fbcom_nr'><div class='wb_nr  content-child-news-nr-body'><div class='userPic content-child-news-nr-img'><img width='38' height='38'src='"+userPicsrc+"'></div><div class='msgBox402 vp-content-child-news-nr'><!--div class='fbcom_yh'></div--><div class='msgCnt'><a href='/UCenter/0/51'>"+userName+"</a>说：<p>"+word+"</p><span class='new'></span></div><div class='pubInfo'><span class='copy_time'>刚刚</span><span class='right'><div class='links'><ul class='links'><li class='comment_reply first last'><a href='/comment/reply/275/272'>回复</a></li></ul></div></span></div></div></div></div></div>";
+		words="<div class='logi_box'><a id='comment-272'></a><div id='272' class='clear' rel='272'><div  class='fbcom_nr'><div class='wb_nr  mjvp-content-child-news-nr'><div class='userPic mjcontent-child-news-nr-img'><img width='38'  height='38'src='"+userPicsrc+"'></div><div class='msgBox402 mjcontent-child-news-nr-body'><!--div class='fbcom_yh'></div--><div  class='msgCnt mjcontent-child-news-nr-title'><a href='/UCenter/0/51' style='color:#0078b6;'>"+userName+"</a>说：<p>"+word+"</p><span class='new'></span></div><div  class='pubInfo'><span class='copy_time'>刚刚</span><span class='right'><div class='links'><ul class='links mjcontent-child-news-nr-tm'><li class='comment_reply  first last mjhhf'><a href='/comment/reply/275/272'>回复</a></li></ul></div></span></div></div></div></div></div>";
 		var flag=$('.box');
 		flag.after(words);
 		var allComments=document.getElementsByClassName('logi_box');
@@ -17,14 +17,18 @@
 			$('.logi_box:last').remove();
 		}
 		var target_url=$(this).parents().find('form').attr('action');
-		var location_url="http://dev.weipujie.com/";
+		var target_form=$(this).parents().find('form');
+		var location_url="http://dev.weipujie.com";
+		//alert(target_form.serialize());
+		
 		$.ajax({
 		   type: "POST",
 		   url: location_url+target_url,
-		   data: "comment=word",
+		   data: target_form.serialize(),
 		   success: function(msg){
-		     //alert( 'DONE' );
+		     alert( 'DONE' );
 		   }
 		});
+		
 	})
 })
